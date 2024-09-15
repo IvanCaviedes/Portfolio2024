7f56e18423b0d74baee2026502ff279fc394762f
Ivan Caviedes <ivan.caviedes@kibernum.com.co>
2024-09-15 15:35:23 -0500

    test: :white_check_mark: fix home test

--- frontend/__tests__/test.tsx
+++ frontend/__tests__/test.tsx
@@ -4,5 +4,5 @@ import Home from '@/pages';
 
 it('first_test', () => {
   render(<Home />);
-  expect(screen.getByText('Learn')).toBeInTheDocument();
+  expect(screen.getByText('Hola')).toBeInTheDocument();
 });
