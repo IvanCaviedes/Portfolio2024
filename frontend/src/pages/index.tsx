d0334e5efcd0a735f2809a31406528bd5d71655c
Ivan Caviedes <36604636+IvanCaviedes@users.noreply.github.com>
2024-09-15 21:34:20 -0500

    feat: :art: add layout blank (#2)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { JSX } from 'react';
 
+import { INextPageWithLayout } from '@/types';
+
 import { fontClass } from './fonts';
 
-export default function Home(): JSX.Element {
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const Home: INextPageWithLayout = (): JSX.Element => {
   return (
     <div className={fontClass}>
       <div className="font-alan">Hola</div>
@@ -10,4 +13,6 @@ export default function Home(): JSX.Element {
       <div className="font-cedarville">Hola 3</div>
     </div>
   );
-}
+};
+
+export default Home;
